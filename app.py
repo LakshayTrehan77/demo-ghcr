30972af1d95f46111c8e2322ed3e98a223355f29
LakshayTrehan77 <lakshay22267@iiitd.ac.in>
2026-02-04 16:08:49 +0530

    Now Working Finally

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Now the File will Finally Work"
+    return "Hello Now it should work properly"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
