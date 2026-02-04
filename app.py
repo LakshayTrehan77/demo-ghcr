353e4d686b7eac639e40f5eb1353cc11e4c62b83
LakshayTrehan77 <lakshay22267@iiitd.ac.in>
2026-02-04 15:57:28 +0530

    Modification Added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from GHCR demo for testing purposes 2 FINALLY PLEASE WORK NOW FINALLYYYYYYYYYY!"
+    return "Hello from GHCR demo for testing purposes 2 FINALLY PLEASE WORK NOW FINALLYYYYYYYYYY 2!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
